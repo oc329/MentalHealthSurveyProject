4c55656dd55fe94197e0aaff0d22022d9bc68c22
oc329 <77870831+oc329@users.noreply.github.com>
2022-05-20 11:49:14 -0700

    Update fileReading.java

--- fileReading.java
+++ fileReading.java
@@ -2,10 +2,25 @@ class fileReading
 {
   public fileReading()
   {
-    
+       BufferedReader bReader = new BufferedReader(new FileReader("foo.in"));
   }
   
-  public string getArrayOfQuestions
+  public String getArrayOfQuestions();
   {
+      String[] questionArray = new String[]; 
+      String current_line = bReader.readLine();
+      questionArray[0] = current_line;
+      int i = 1;
+      while(current_line != null)
+      {
+          current_line = bReader.readLine();
+          if(current_line != ""){
+            questionArray[i] = current_line;
+            i++
+          {
+            
+      }
+      return questionArray;
   }
 }
+    
